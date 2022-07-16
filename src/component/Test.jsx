d85d0da07e906517f8bd2263eb07b92d783d6f76
niubingchuan <niubingchuan@enn.cn>
2022-07-17 01:04:26 +0800

    build: 配置环境变量

--- src/component/Test.jsx
+++ src/component/Test.jsx
@@ -1,11 +1,21 @@
 import Logo from 'assets/logo.svg'
+import React, { useState } from 'react'
 
-
+console.log(process.env);
 export default function Test(props) {
+  const [user,setUser] = useState(['1','2'])
+  const change = () => {
+    setUser(['3',...user])
+  }
+
   return (
     <>
       <p>test</p>
-      <img src={Logo} alt="" />
+      <img src={Logo} alt=""  onClick={change} style={{width: '100px'}}/>
+      {user.map((item,index) => (<React.Fragment key={item}>
+        <p>{item}</p>
+        <input type="text"/>
+      </React.Fragment>))}
     </>
   )
 }
